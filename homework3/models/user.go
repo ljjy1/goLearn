b713a9bc3fbdfcef5b4b20abf27620645a69c0c5
zgp <zengguangpingqaz@163.com>
2026-01-03 00:05:46 +0800

    优化索引位置

--- homework3/models/user.go
+++ homework3/models/user.go
@@ -14,11 +14,11 @@ type User struct {
 	ID        uint `gorm:"primarykey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt soft_delete.DeletedAt `gorm:"uniqueIndex:idx_mobile_deleted_at"`
 	Nickname  string                `json:"nickname" gorm:"not null;size:20;comment:昵称"`                                  //设置不能为null 长度20 唯一索引
 	Mobile    string                `json:"mobile" gorm:"not null;size:11;comment:手机号;uniqueIndex:idx_mobile_deleted_at"` //设置不能为null 长度11 唯一索引要和DeletedAt关联
 	Age       *uint8                `json:"age" gorm:"comment:年龄"`                                                        //年龄可以为空 使用指针避免查询返回0
 	PostCount uint32                `json:"postCount" gorm:"default:0;comment:文章数量"`                                      //默认值为0
+	DeletedAt soft_delete.DeletedAt `gorm:"uniqueIndex:idx_mobile_deleted_at"`
 
 	//关联文章模型 一对多关系 外键为UserID 引用为ID
 	Posts []Post `json:"posts" gorm:"foreignKey:UserID;references:ID;comment:文章"`
